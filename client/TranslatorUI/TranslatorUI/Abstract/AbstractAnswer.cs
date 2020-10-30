cc3fc2b1f808ecb5ab1241caf65c06b2a2a0cd0d
展麒 邓 <429503410@qq.com>
2020-10-30 19:00:03 +0800

    add factory mode

---
+++ client/TranslatorUI/TranslatorUI/Abstract/AbstractAnswer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslatorUI.Abstract
+{
+    public class DisplayAnswer : Answer
+    {
+        /*已在抽象类中声明
+        public bool ShowAdoptBtn { get; set; }
+        public bool IsMyAnswer { set; get; }*/
+        public override void convert(Answer answer)     //已在factory中实现，等待删除
+        {
+            this.AnswerId = answer.AnswerId;
+            this.Content = answer.Content;
+            this.CreateTime = answer.CreateTime;
+            this.IsAdopted = answer.IsAdopted;
+            this.UserId = answer.UserId;
+            this.AnswerId = answer.AnswerId;
+            this.Like = answer.Like;
+        }
+        public DisplayAnswer()
+        {
+            // this.adoptButton = false;
+            // this.ismyanswer = false;
+        }
+    }
+}
